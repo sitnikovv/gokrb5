ed08b7df50f440d6b6ab794a903d81e8e9d0ef17
Vyacheslav Sitnikov <vsitnikov@sberbank.ru>
2024-08-19 14:26:20 +0300

    Change external resolver functional. Again

--- v8/config/krb5conf_secman.go
+++ v8/config/krb5conf_secman.go
@@ -4,6 +4,6 @@ import "stash.delta.sbrf.ru/secman/vault-plugin-helpers/network"
 
 type External struct {
 	ExternalResolvers     []network.ResolverIf
-	ExternalResolversRule string
+	ExternalResolversRule network.ResolverRule
 	Discovery             network.Discovery
 }
