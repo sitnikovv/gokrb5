c72a988b73fa2f06df1b1c80f8a6330a2f13dfc6
Vyacheslav Sitnikov <vsitnikov@sberbank.ru>
2024-08-20 10:30:26 +0300

    Refactoring

--- v8/config/hosts_secman.go
+++ v8/config/hosts_secman.go
@@ -1,8 +1,8 @@
 package config
 
 import (
+	"github.com/jcmturner/dnsutils/v2"
 	"net"
-	"stash.delta.sbrf.ru/secman/vault-plugin-helpers/network"
 )
 
 func (c *Config) OrderedSRV(service, proto, realm string) (int, map[int]*net.SRV, error) {
@@ -13,17 +13,23 @@ func (c *Config) OrderedSRV(service, proto, realm string) (int, map[int]*net.SRV
 
 	serviceName := service + "://" + realm
 	external := c.External.Discovery
+
+	// Если callback на внешнюю функцию исследования не указан, будем использовать оригинальное поведение
 	if external == nil || external.ServiceDiscovery() == nil {
-		list, err = network.ServiceDiscoveryOrdered(serviceName, c.External.ExternalResolvers, c.External.ExternalResolversRule, proto)
-	} else {
-		list, err = external.ServiceDiscovery()(serviceName, c.External.ExternalResolvers, c.External.ExternalResolversRule, proto)
+		return dnsutils.OrderedSRV("kerberos", proto, realm)
 	}
+
+	// Вызываем внешнюю функцию для исследования сети
+	list, err = external.ServiceDiscovery()(serviceName, c.External.ExternalResolvers, c.External.ExternalResolversRule, proto)
 	if err != nil {
 		return 0, nil, err
 	}
+
+	// На основе полученных данных формируем выходной результат
 	result := map[int]*net.SRV{}
 	for i, srv := range list {
 		result[i+1] = srv
 	}
+
 	return len(result), result, err
 }
